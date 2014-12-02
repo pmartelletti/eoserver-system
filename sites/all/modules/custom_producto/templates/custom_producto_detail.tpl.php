15274fd95b66832b9db66c01210b59775a0a8127
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2014-12-01 23:29:39 -0300

    Orden Alfabetico

--- sites/all/modules/custom_producto/templates/custom_producto_detail.tpl.php
+++ sites/all/modules/custom_producto/templates/custom_producto_detail.tpl.php
@@ -65,14 +65,33 @@
         if (!empty($field['#items'])):
             ?>
             <div class="imagenes">
-                <?php
-                foreach ($field['#items'] as $item):
-                    $image_principal = image_style_url('productos_imagen_ambientes', $item['uri']);
-                    $image_zoom = file_create_url($item['uri']);
-                    ?>
-                    <img class="images_zoom" src="<?php print $image_principal ?>" data-zoom-image="<?php print $image_zoom ?>"/>
-                <?php endforeach; ?>
+                <div class="triggerAnimation animated" data-animate="fadeInLeft">
+                    <ul id='services-carousel' class='carousel-li'>
+                        <?php
+                        foreach ($field['#items'] as $item):
+                            $image_principal = image_style_url('productos_imagen_ambientes', $item['uri']);
+                            $image_zoom = file_create_url($item['uri']);
+                            ?>
+                            <li class="service-box-1"><img class="images_zoom" src="<?php print $image_principal ?>" data-zoom-image="<?php print $image_zoom ?>"/></li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <div class="clearfix"></div>
+                    <?php if(count($field['#items'])> 1):?>
+                    <ul class="carousel-nav">
+                        <li>
+                            <a class="c_prev" href="#"></a> 
+                        </li>
+                        <li>
+                            <a class="c_next" href="#"></a>
+                        </li>
+                    </ul>
+                    <?php endif;?>
+                </div><!-- .triggerAnimation animated end -->
+
             </div>
+
+
+
         <?php endif ?>
         <?php
         $block = module_invoke('views', 'block_view', 'productos_misma_categoria-block');

8988bf0ce6ae02df4f6520894c8a1208c2b27aee
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2014-12-03 00:49:16 -0300

    Galerias

---
+++ sites/all/themes/elvyre/templates/_field--field-imagenes--gallery.tpl.php
@@ -0,0 +1,9 @@
+<?php
+$a=0;
+die('a');
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+

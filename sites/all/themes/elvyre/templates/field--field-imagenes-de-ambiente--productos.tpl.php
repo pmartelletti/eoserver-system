9e517e13548d2b9fd697cb1c047651d3ba2a7983
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2014-12-02 23:36:01 -0300

    Pagination

--- sites/all/themes/elvyre/templates/field--field-imagenes-de-ambiente--productos.tpl.php
+++ sites/all/themes/elvyre/templates/field--field-imagenes-de-ambiente--productos.tpl.php
@@ -1,7 +1,9 @@
-<ul id="ambientes-carousel">
-    <?php foreach ($items as $item): ?>
-        <li><?php print render($item) ?></li>
-    <?php endforeach; ?>
-</ul>
-<div class="clearfix"></div>
-<div id="ambientes-pagination"></div>
+<div class="field-imagenes-de-ambientes">
+    <ul id="ambientes-carousel">
+        <?php foreach ($items as $item): ?>
+            <li><?php print render($item) ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <div class="clearfix"></div>
+    <div id="ambientes-pagination"></div>
+</div>

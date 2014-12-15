b79f1072a75b533f0062efb27d6b0b1c87317baa
Your Name <you@example.com>
2014-12-15 13:14:25 -0500

    Lightbox Gallery

--- sites/all/modules/custom_producto/custom_producto_flip_image/template/custom-producto-flip-image-field.tpl.php
+++ sites/all/modules/custom_producto/custom_producto_flip_image/template/custom-producto-flip-image-field.tpl.php
@@ -1,7 +1,13 @@
 <div <?php isset($item['field_details'][LANGUAGE_NONE][0]) ? print "class=\"flip-container\" ontouchstart=\"this.classList.toggle('hover');\"" : print 'class="no-flip-container" '?>>
     <div class="flipper">
         <div class="front">
-            <?php print theme('image_style', array('style_name' => 'galerias_galeria', 'path' => $item['uri'])) ?>
+            <?php //print theme('image_style', array('style_name' => 'galerias_galeria', 'path' => $item['uri'])) ?>
+            <?php
+            print theme('lightbox2_image', array(
+            		'image_style' => 'galerias_galeria', 
+            		'item' => $item,
+            		'lightbox_type'=>'lightbox',
+            )); ?>
         </div>
         <?php if (isset($item['field_details'][LANGUAGE_NONE][0])): ?>
             <div class="back">
@@ -10,3 +16,4 @@
         <?php endif ?>
     </div>
 </div>
+<!-- lightbox2__lightbox__thumbnail__original  -->

999b0b035f3c8639f5a27247bffeacfeb4c2280d
Rodrigo <rspirolazzi@gmail.com>
2014-12-16 14:59:13 -0300

    Galeria

--- sites/all/modules/custom_producto/custom_producto_flip_image/template/custom-producto-flip-image-field.tpl.php
+++ sites/all/modules/custom_producto/custom_producto_flip_image/template/custom-producto-flip-image-field.tpl.php
@@ -3,10 +3,12 @@
         <div class="front">
             <?php //print theme('image_style', array('style_name' => 'galerias_galeria', 'path' => $item['uri'])) ?>
             <?php
+            //var_dump($item);exit;
+            $item['title'] = isset($item['field_details'][LANGUAGE_NONE][0]) ? $item['field_details'][LANGUAGE_NONE][0]['value'] : $item['title'];
             print theme('lightbox2_image', array(
             		'image_style' => 'galerias_galeria', 
             		'item' => $item,
-            		'lightbox_type'=>'lightbox',
+            		'lightbox_type'=>'lightbox[galeria]',
             )); ?>
         </div>
         <?php if (isset($item['field_details'][LANGUAGE_NONE][0])): ?>

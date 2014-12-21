c1a0bd2ff0292e98677f69b0b4b94078db6e6f90
Andres <andresmary@gmail.com>
2014-12-21 17:53:24 -0600

    css mobile

--- sites/all/modules/custom_producto/templates/custom_filter_all_mobile.tpl.php
+++ sites/all/modules/custom_producto/templates/custom_filter_all_mobile.tpl.php
@@ -1,5 +1,5 @@
 <h2><?php print $category ?></h2>
-<a class="display_mobile_menu" href="#"><img src="/sites/all/themes/elvyre/img/disp.jpg"></a>
+<a class="display_mobile_menu" href="#"><img src="/sites/all/themes/elvyre/img/disp.png"></a>
 <div clas="mobile_menu">
     <?php print $list;?>
 </div>

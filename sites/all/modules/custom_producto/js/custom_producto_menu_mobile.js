c06596e88d570454a756612da334f3f3ce8c648b
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2014-12-21 21:49:42 -0300

    Javascript

--- sites/all/modules/custom_producto/js/custom_producto_menu_mobile.js
+++ sites/all/modules/custom_producto/js/custom_producto_menu_mobile.js
@@ -1,11 +1,22 @@
 (function ($) {
     Drupal.behaviors.CustomProductoMenu = {
         attach: function (context, settings) {
-            $('a.display_mobile_menu').click(function (e) {
+            $('a.display_mobile_menu').not('.processed').click(function (e) {
                 e.preventDefault();
-                //$(".productos_filtro_categoria ul").toggle("slow");
+                $('.mobile_menu > div.item-list > ul').toggle("slow");
                 //$(".productos_filtro_linea ul").toggle("slow");
                 //$(".muestra_producto .opciones").toggle("slow");
+            }).addClass('processed');
+            $('.mobile_menu > div.item-list > ul > li').not('.processed').each(function (index, element) {
+                $(element).addClass('processed');
+                $('> a', element).click(function (e) {
+                    e.preventDefault();
+                    var ul = $(this).next().find('> ul');
+                    $(ul).toggle('show', function(){
+                        $('.mobile_menu > div.item-list > ul > li > div.item-list > ul').not(ul).hide();
+                    });
+                    
+                });
             });
         }
     };

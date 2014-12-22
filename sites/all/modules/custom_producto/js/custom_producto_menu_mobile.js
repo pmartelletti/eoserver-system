0b2743f0ca18886efcb052712285e20d0b3d5e24
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2014-12-21 22:27:48 -0300

    Menu elemento seleccionado

--- sites/all/modules/custom_producto/js/custom_producto_menu_mobile.js
+++ sites/all/modules/custom_producto/js/custom_producto_menu_mobile.js
@@ -1,13 +1,13 @@
 (function ($) {
     Drupal.behaviors.CustomProductoMenu = {
         attach: function (context, settings) {
-            $('a.display_mobile_menu').not('.processed').click(function (e) {
+            $('a.display_mobile_menu', context).not('.processed').click(function (e) {
                 e.preventDefault();
                 $('.mobile_menu > div.item-list > ul').toggle("slow");
                 //$(".productos_filtro_linea ul").toggle("slow");
                 //$(".muestra_producto .opciones").toggle("slow");
             }).addClass('processed');
-            $('.mobile_menu > div.item-list > ul > li').not('.processed').each(function (index, element) {
+            $('.mobile_menu > div.item-list > ul > li', context).not('.processed').each(function (index, element) {
                 $(element).addClass('processed');
                 $('> a', element).click(function (e) {
                     e.preventDefault();
@@ -18,6 +18,17 @@
                     
                 });
             });
+            if($('.mobile_menu:not(.processed) a.active', context).length > 0 ){
+                var div = $('<div>').addClass('selected');
+                
+                    $(div).append($('.mobile_menu a.active', context).parents().eq(3).find('>a').clone());
+                    $(div).append($('.mobile_menu a.active', context).clone());
+                
+                
+                
+                $(div).insertAfter($('.mobile_menu', context));
+                $('.mobile_menu', context).addClass('processed');
+            }
         }
     };
 })(jQuery);
